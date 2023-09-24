994e71b59c101882ecd93df66b635d9e95bf6148
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-09-24 18:46:14 +0300

    complete first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const refs = {
+  renderedGalleryList: document.querySelector(".gallery"),
+  modalWindow: document.querySelector("div.basicLightbox")
+};
+
+// =========================================================
+
+
+function imgTemplate (el) {
+  return `<li class="gallery__item">
+    <a class="gallery__link" href="${el.original}">
+      <img
+      class="gallery__image"
+      src="${el.preview}" 
+      alt="${el.description}"
+      />
+    </a>
+</li>`
+}
+
+
+function renderGalleryElFromTemplate() {
+  const markup = galleryItems.map(imgTemplate).join("");
+  refs.renderedGalleryList.innerHTML = markup;
+}
+
+renderGalleryElFromTemplate();
+
+// ========================================================
+
+const lightbox = new SimpleLightbox('.gallery a', {captions: true, captionSelector: "img", captionsData: "alt", captionDelay: 250,});
