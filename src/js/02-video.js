7dc3ac5438926b28bd0a08f8e7423c8f0f196aa7
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-09-24 21:25:54 +0300

    complete second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// =============================================
+
+const refs = {
+  playerVimeo: document.getElementById('vimeo-player'),
+  CURRENT_TIME: 'videoplayer-current-time',
+};
+
+// ====================================================
+
+const player = new Player(refs.playerVimeo);
+
+const saveCurrentTime = function (ev) {
+  localStorage.setItem(refs.CURRENT_TIME, ev.seconds)
+};
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+
+// ===================================================
+
+
+player.setCurrentTime(localStorage.getItem(refs.CURRENT_TIME) || 0);
+
+// =====================================================
+
+player.getEnded().then(ev => 
+  localStorage.removeItem(refs.CURRENT_TIME))
+
 
